e442bd8dc6f13962810575062b99c17e3a00ba90
Wangzhihui Mei <wangzhihui.mei@intel.com>
2021-12-07 16:57:20 +0800

    add redis connection

--- src/queueworker.ts
+++ src/queueworker.ts
@@ -1,21 +1,30 @@
-import { Queue, Worker, QueueEvents } from 'bullmq'
+import { Queue, Worker, QueueEvents, RedisConnection } from 'bullmq'
 import { spawn } from 'child_process'
 import { createBullBoard } from 'bull-board'
 import { BullMQAdapter } from 'bull-board/bullMQAdapter'
 import * as os from 'os'
+import * as Redis from 'ioredis'
+import * as dotenv from 'dotenv'
+dotenv.config({ path: '../.env', debug: true })
 
 const { router: bullBoardRouter, addQueue, removeQueue } = createBullBoard([])
+const host = process.env.REDIS_HOST
+const port = Number(process.env.REDIS_PORT)
+console.log(host, port)
+const connection = new Redis(6379, 'tracing100.sh.intel.com', {
+  maxRetriesPerRequest: null,
+  enableReadyCheck: false
+})
 
 class UserQueue {
   queueName: string
   queue: Queue
   queueWorker: Worker
   queueEvents: QueueEvents
-
   constructor (queueName: string) {
     this.queueName = queueName
 
-    this.queue = new Queue(this.queueName)
+    this.queue = new Queue(this.queueName, { connection })
 
     this.queueWorker = new Worker(
       this.queueName,
@@ -40,10 +49,10 @@ class UserQueue {
         job.updateProgress(100)
         console.log(`-----${job.name}-----done-----\n`)
       },
-      { concurrency: os.cpus().length }
+      { concurrency: os.cpus().length, connection }
     )
 
-    this.queueEvents = new QueueEvents(this.queueName)
+    this.queueEvents = new QueueEvents(this.queueName, { connection })
     this.queueEvents.on('completed', jobId => {
       console.log('-----done-----\n')
     })
